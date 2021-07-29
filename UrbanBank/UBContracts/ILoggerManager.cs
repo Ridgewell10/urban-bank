01931bb026286934f7fd8079a4d87c6ae85eb478
Ridgewell <ridgewell@ava.solutions>
2021-07-30 01:19:59 +0200

    addded contracts project logging  (isuus with line 9)

---
+++ UrbanBank/UBContracts/ILoggerManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UBContracts
+{
+    public interface  ILoggerManager
+    {
+            void LogInfo(string message);
+            void LogWarn(string message);
+            void LogDebug(string message);
+            void LogError(string message);
+        
+    }
+}
---
+++ UrbanBank/UBLoggingService/LoggerManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using UBContracts;
+
+namespace UBLoggingService
+{
+    public class LoggerManager :ILoggerManager
+    {
+        private static readonly Microsoft.Extensions.Logging.ILogger logger = (Microsoft.Extensions.Logging.ILogger)LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            logger.LogDebug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.LogError(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.LogInformation(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.LogWarning(message);
+        }
+    }
+}
