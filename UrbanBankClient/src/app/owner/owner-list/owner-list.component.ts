a1b8a419dc5bd06b2ba170b6a387dca6c01a68f5
Ridgewell <ridgewell@ava.solutions>
2021-12-27 20:45:48 +0200

    been a while

--- UrbanBankClient/src/app/owner/owner-list/owner-list.component.ts
+++ UrbanBankClient/src/app/owner/owner-list/owner-list.component.ts
@@ -1,8 +1,8 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { RepositoryService } from './../../shared/services/repository.service';
 import { Owner } from './../../_interfaces/owner.model';
 import { ErrorHandlerService } from './../../shared/services/error-handler.service';
-
 
 @Component({
   selector: 'app-owner-list',
@@ -11,18 +11,28 @@ import { ErrorHandlerService } from './../../shared/services/error-handler.servi
 })
 export class OwnerListComponent implements OnInit {
   public owners!: Owner[];
+  public errorMessage: string = '';
 
-  constructor(private repository: RepositoryService) { }
+  constructor(private repository: RepositoryService, private errorHandler: ErrorHandlerService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getAllOwners();
   }
 
   public getAllOwners = () => {
     let apiAddress: string = "api/owner";
     this.repository.getData(apiAddress)
-    .subscribe(res => {
-      this.owners = res as Owner[];
-    })
+      .subscribe(res => {
+        this.owners = res as Owner[];
+      },
+      (error) => {
+        this.errorHandler.handleError(error);
+        this.errorMessage = this.errorHandler.errorMessage;
+      })
+  }
+
+  public getOwnerDetails = (id: any) => {
+    const detailsUrl : string =  `/owner/details/${id}`;
+    this.router.navigate([detailsUrl]);
   }
 }
